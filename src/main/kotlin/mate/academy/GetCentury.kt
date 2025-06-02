13a871ee14b78ad7ac6128b187fd2a757ac1d8f2
Bohdan Lysachenko <lysachenko111@ukr.net>
2025-06-02 14:37:24 +0300

    Solution

--- src/main/kotlin/mate/academy/GetCentury.kt
+++ src/main/kotlin/mate/academy/GetCentury.kt
@@ -1,5 +1,8 @@
 package mate.academy
 
-fun getCentury(year: Int) : Int {
-    return 0
+private const val OFFSET = 99
+private const val DIVISOR = 100
+
+fun getCentury(year: Int): Int {
+    return (year + OFFSET) / DIVISOR
 }
